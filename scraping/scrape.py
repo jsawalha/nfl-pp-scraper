7915a60a552b4a928fa4cfe1ccf23f67cf6bd496
jsawalha <jsawalha@ualberta.ca>
2022-11-22 10:19:26 -0700

    added a config flag for population indexing

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -10,14 +10,18 @@ with open("utils/profile_config.yaml", "r") as f:
 
 
 # Setting config parameters
+# Position setting
 pos = config["profile_options"]["pos"]
 
+# Headers and cookies setting
 headers = config["urlParams"]["headers"]
 cookies = config["urlParams"]["cookies"]
 
-# Scrape links again?
-# If true, it will crawl all car links based on the search
-# If false, it will skip and use the most recently saved file
+# Popularity Index flag
+pop_index = config["profile_options"]["pop_index"]
+
+
+# Scrape links again
 scrape_link_bool = config["scrape_links"]
 
 
@@ -25,17 +29,17 @@ URL = f"https://www.playerprofiler.com/position/{pos}"
 
 
 def main():
-    
+
     # Initialize instance for web crawler
     auto_web = web_crawler(url=URL, headers=headers, cookies=None)
 
     # Determine the links for all players on playerprofiler.com
     page_links = auto_web.getNameLinks(
-        pop_index=True, save=True, scrape_links=scrape_link_bool
+        pop_index=pop_index, save=True, scrape_links=scrape_link_bool
     )
 
     # Retreive all the car links on a given page
-    df = auto_web.scrapePage(page_list=page_links)
+    auto_web.scrapePage(page_list=page_links)
 
     print("Finished")
 
